88120d687a3a8e1a636f74517d4232500fbd7188
manishnarmala07-cmyk <manishnarmala07@gmail.com>
2026-06-21 00:38:14 +0530

    Added root route redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route
+  path="/"
+  element={
+    <Navigate
+      to="/login"
+      replace
+    />
+  }
+/>
+        <Route
           path="/login"
           element={<Login />}
         />
